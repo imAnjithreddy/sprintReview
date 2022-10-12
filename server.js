1b7171f7a26741b69550ada8c704eec9b4b7ed4f
Anjith Reddy <anjithreddy5@gmail.com>
2022-10-12 22:42:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
 const express = require("express");
 const app = express();
-var cors = require('cors');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-//DB from config
-const db = require("./config/keys").mongoURI;
-//Connection to mongoose
-mongoose
-.connect(db)
-.then(()=>{console.log("MongoDB connected")})
-.catch(err => console.log(err));
-
-
-const port = 8081;
-//Passport middleware
-app.use(passport.initialize());
-
-//Passport Config
-
-require("./config/passport")(passport);
-
-app.use("/api/users",users);
-app.use("/api/profile",profile);
-app.use("/api/posts",posts);
+const port = process.env.PORT || 8081;
 
 app.listen(port,()=>{
     console.log(`Server running on ${port}`);
